14d28612389aff1bee6d64aa37c284899f14375a
Vandaci <vmex7@outlook.com>
2022-07-18 08:36:00 +0800

    将AlexNet改为内置一致

--- AlexNet/alexnet.py
+++ AlexNet/alexnet.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torchsummary import summary
 
 
 # 输入为3×224×224
@@ -8,22 +9,20 @@ class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
         self.sequential = nn.Sequential(
-            nn.Conv2d(3, 96, 11, stride=4),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(3, 64, 11, stride=4, padding=2), nn.ReLU(inplace=True),
             nn.MaxPool2d(3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 384, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, 3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 384, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.AdaptiveAvgPool2d((6, 6)),
             nn.Flatten(),
-            nn.Linear(256 * 5 * 5, 4096),
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, 1000)
@@ -38,6 +37,8 @@ if __name__ == "__main__":
 
     net = models.alexnet()
     model = AlexNet()
-    x = torch.randn(1, 3, 224, 224)
-    y = model(x)
+    summary(model, (3, 224, 224), device='cpu')
+    summary(net, (3, 224, 224), device='cpu')
+    # x = torch.randn(1, 3, 224, 224)
+    # y = model(x)
     pass
---
+++ utils/viewshape.py
@@ -0,0 +1,8 @@
+# Time    : 2022.07.18 上午 08:04
+# Author  : Vandaci(cnfendaki@qq.com)
+# File    : viewshape.py
+# Project : ClassicNetwork
+
+
+
+
